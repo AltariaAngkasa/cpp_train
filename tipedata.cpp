61fff8c61a94e00656ddb093724f070263df1185
Alif Wilah Desfitrawan <36957119+Alifwilah@users.noreply.github.com>
2022-10-28 19:18:41 +0700

    Create tipedata.cpp

---
+++ tipedata.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x = 2;
+	float y = 2.5;
+	string z = "Hello World";
+	char a = 'H';
+	bool b = true;
+	
+	cout<<"Integer: "<<x<<endl;
+	cout<<"Float: "<<y<<endl;
+	cout<<"String: "<<z<<endl;
+	cout<<"Character: "<<a<<endl;
+	cout<<"Boolean: "<<b<<endl;
+}
